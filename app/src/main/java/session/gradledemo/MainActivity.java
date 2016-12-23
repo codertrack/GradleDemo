992ee39eb87cfe503756ca2b8e0d7185a8048d0d
wukai <wukai@wukai.local>
2016-12-23 14:19:53 +0800

    Initial commit 我的初始化项目

---
+++ app/src/main/java/session/gradledemo/MainActivity.java
@@ -0,0 +1,18 @@
+package session.gradledemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void getName(View view) {
+		System.out.println(getPackageName());
+	}
+}
